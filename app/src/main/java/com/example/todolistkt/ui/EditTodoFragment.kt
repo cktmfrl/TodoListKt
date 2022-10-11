f312690192df26ab7d33f9d0096a387401084820
origin <sskkccha@gmail.com>
2022-10-11 16:27:50 +0900

    레이아웃 완성

--- app/src/main/java/com/example/todolistkt/ui/EditTodoFragment.kt
+++ app/src/main/java/com/example/todolistkt/ui/EditTodoFragment.kt
@@ -1,4 +1,4 @@
-package com.example.todolistkt
+package com.example.todolistkt.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,17 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.todolistkt.databinding.FragmentSecondBinding
+import com.example.todolistkt.R
+import com.example.todolistkt.databinding.FragmentEditTodoBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class EditTodoFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentEditTodoBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,7 +19,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentEditTodoBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +27,8 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.doneFab.setOnClickListener {
+            findNavController().navigate(R.id.action_EditFragment_to_ListFragment)
         }
     }
 
